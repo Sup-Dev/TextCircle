7eb35712929526b0e70d8f73d64a88603db491f5
Sourabh Dev <sourabh.dev90@gmail.com>
2015-12-17 22:46:57 +0530

    chore: added mongo collection named documents and a startup script

--- textcircle.js
+++ textcircle.js
@@ -1,9 +1,15 @@
+this.Documents = new Mongo.Collection("documents");
+
 if (Meteor.isClient) {
 
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function () {
+        // code to run on server at startup
+        if (!Documents.findOne()) {
+            Documents.insert({title: "my new document"});
+        }
+
+    });
 }
