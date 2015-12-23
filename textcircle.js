d61d1b934e69a863379e5e3bb30b4d19573b1776
Sourabh Dev <sourabh.dev90@gmail.com>
2015-12-23 21:30:20 +0530

    feat: app now shows last edit date and first name of the user

--- textcircle.js
+++ textcircle.js
@@ -24,6 +24,28 @@ if (Meteor.isClient) {
             }
         }
     });
+
+    Template.editingUsers.helpers({
+        users: function() {
+            var doc, eusers, users;
+            doc = Documents.findOne();
+            if (!doc) {
+                return;
+            }
+            eusers = EditingUsers.findOne({docid: doc._id});
+            if (!eusers) {
+                return;
+            }
+
+            users = new Array();
+            var i = 0;
+            for (var user_id in eusers.users) {
+                users[i] = fixObjectKeys(eusers.users[user_id]);
+                i++;
+            }
+            return users;
+        }
+    });
 }
 
 if (Meteor.isServer) {
@@ -64,3 +86,12 @@ Meteor.methods({
         EditingUsers.upsert({_id:eusers._id}, eusers);
     }
 });
+
+function fixObjectKeys(obj) {
+    var newObj = {};
+    for (key in obj) {
+        var key2 = key.replace("-", "");
+        newObj[key2] = obj[key];
+    }
+    return newObj;
+}
